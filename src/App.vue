024fd4f8b9c5adaa700acfe9e3c784a64d6a6efc
sie_morishita <koichi.morishita@ssie.jp>
2020-04-16 02:08:21 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DrawTool />
   </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import DrawTool from './components/DrawTool.vue'
+ 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DrawTool
   }
 }
 </script>
